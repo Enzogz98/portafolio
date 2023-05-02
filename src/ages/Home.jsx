fda021b2ee6ab72b687843643ad9f9ef63edb305
Enzogz98 <enzo.114998@gmail.com>
2023-05-02 15:13:34 -0300

    unificacion de comonentes

---
+++ src/ages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../comonentes/Header'
+import Main from '../comonentes/Main'
+import Footer from '../comonentes/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/comonentes/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import foto from "../assets/foto.jpg"
+
+
+
+const Header = () => {
+  return (
+  
+    <div>
+      <img src={foto} alt="111" srcset="122" />
+      <h1> Soy Enzo Gonzalez, estudiante de la tecnicatura universitaria en rogramacion, de la UTN en la FRT. Actualmente manejo los lenguajes de rogramacion de C# y JavaScrit.</h1>
+    </div>
+  )
+}
+
+export default Header
